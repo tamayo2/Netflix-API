aacc3a48eb2858c95ddda85fedfc4fcd789dd126
tamayo2 <juan.tamayo3@utp.edu.co>
2025-03-09 23:46:40 -0500

    Canalizacion de usuarios

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -5,6 +5,12 @@ const Player = () => {
     return (
         <div className='player'>
             <img src={back_arrow_icon} alt=""/>
+            <iframe width='90%' height='90%' src='https://www.youtube.com/embed/VxlSFWb2hwg' title='trailer' frameBorder='0' allowFullScreen></iframe>
+            <div className='player-info' >
+                <p>Published Date</p>
+                <p>Name</p>
+                <p>Type</p>
+            </div>
         </div>
     )
 }
