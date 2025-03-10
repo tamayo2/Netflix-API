b95befcc62c932cf6fc1c481e551f66e074136f8
tamayo2 <juan.tamayo3@utp.edu.co>
2025-03-10 00:29:19 -0500

    Actualizacion Navbar

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -32,7 +32,7 @@ const Player = () => {
 
     return (
         <div className='player'>
-            <img src={back_arrow_icon} alt=""/>
+            <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
             <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder='0' allowFullScreen></iframe>
             <div className='player-info' >
                 <p>{apiData.published_at.slice(0,10)}</p>
